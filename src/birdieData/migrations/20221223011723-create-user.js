02fd17d83aab6abcee54b2b019a52b9472050136
Tim Lenahan <tlenahan14@gmail.com>
2022-12-23 17:27:24 -0700

    init sequelize

---
+++ src/birdieData/migrations/20221223011723-create-user.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      stripeCustomerId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      stripeAccountId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      requiresStripeWork: {
+        allowNull: false,
+        defaultValue: true,
+        type: Sequelize.BOOLEAN,
+      }, 
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
